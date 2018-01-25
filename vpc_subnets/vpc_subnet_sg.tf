3a90218d3ed8c5b2478b664c73e18b6e44d71a50
Anuroop Krishna <anuroop.krishna@rubyseven.net>
2018-01-25 14:37:39 +0530

    changed ipv6 security group settings

--- vpc_subnets/vpc_subnet_sg.tf
+++ vpc_subnets/vpc_subnet_sg.tf
@@ -39,13 +39,13 @@ resource "aws_security_group" "game_http_ipv6_inbound" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
   vpc_id = "${aws_vpc.game_vpc.id}"
   tags {
